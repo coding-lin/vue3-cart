7b43c3a088278ab8de358c44ba3591b38e2909a0
hjl <2764881569@qq.com>
2023-03-18 15:04:53 +0800

    基于axios请求商品列表数据

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,47 @@
-<script setup>
-
-</script>
-
 <template>
-  
+  <div class="app-container">
+    <h1>App 根组件</h1>
+    <hr/>
+    <es-header title="购物车案例"></es-header>
+  </div>
 </template>
 
-<style scoped>
+<script>
+// 导入 header 组件
+import EsHeader from './components/es-header/EsHeader.vue';
 
+export default {
+  name:'MyApp',
+  data() {
+    return {
+      // 商品列表的数据
+      goodslist: [],
+    }
+  },
+  // 组件实例创建完毕之后的生命周期函数
+  created() {
+    // 调用 methods 中的 getGoodsList 方法，请求商品列表的数据
+    this.getGoodsList()
+  },
+  methods: {
+    // 请求商品列表的数据
+    async getGoodsList() {
+      // 通过组件实例 this 访问到全局挂载的 $http 属性，并发起 Ajax 请求
+      const {data: res} = await this.$http.get('/api/cart');
+      // 判断是否请求成功
+      if (res.status !== 200) return alert('请求商品列表数据失败');
+      this.goodslist = res.list;
+    }
+  },
+  components: {
+    // 注册 header 组件
+    EsHeader
+  }
+}
+</script>
+
+<style scoped>
+.app-container {
+  padding-top: 45px;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,15 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+// import "./assets/css/bootstrap.css";
 import "./index.css";
 
-createApp(App).mount("#app");
+import axios from "axios";
+
+const app = createApp(App);
+
+// 配置请求的根路径
+axios.defaults.baseURL = "https://www.escook.cn";
+// 将 axios 挂载为全局的 $http 自定义属性
+app.config.globalProperties.$http = axios;
+
+app.mount("#app");
